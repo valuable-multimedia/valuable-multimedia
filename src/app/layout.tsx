f6c32ae9dc6b9e787fa23ac642bbac5ef062a3b2
jay rai <jayrai3003@gmail.com>
2024-07-12 11:43:36 +0530

    updating main page stickers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Sora } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import Link from "next/link";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 const sora = Sora({ subsets: ["latin"] });
 
@@ -20,7 +22,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={sora.className}>
         <Navbar />
+        <div className="sticker fixed top-[50%] -right-[4.5rem] rounded-lg py-3 text-white bg-themeOrange rotate-90">
+          <Link href={"/contact-us"} className="text-lg px-4">
+          FREE Digital Audit
+          </Link>
+        </div>
         {children}
+        <div className="sticker fixed bottom-1 right-1 text-white rounded-full p-4 text-3xl z-[100] bg-[#00d25c] hover:scale-[1.05]">
+          <Link href={`https://wa.me/${"+919016850552"}?text=${"Hii"}`}>
+            <IoLogoWhatsapp />
+          </Link>
+        </div>
         <Footer />
       </body>
     </html>
